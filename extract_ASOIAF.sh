06bc94c45fc831e6de1301a8f6cfef00db0508b2
Joe Bullard <jtb4478@cs.rit.edu>
2014-07-12 19:10:10 -0400

    Fixed issue with multi-worded character names.

--- extract_ASOIAF.sh
+++ extract_ASOIAF.sh
@@ -4,7 +4,7 @@
 # 
 # Lines of text are continuously dumped, but the output changes to
 #'book/character_number.txt' at each chapter header, where 'number'
-# is incremented each time the charcater is encountered
+# is incremented each time the character is encountered
 #
 ################################################################################
 
@@ -22,6 +22,8 @@ declare -A ABBRV=( [a_game_of_thrones]=AGOT
                    [a_storm_of_swords]=ASOS
                    [a_feast_for_crows]=AFFC )
 
+in_book=0
+
 # Run on all books
 for book in ${DIR_TEXTS}/*.txt
 do
@@ -38,49 +40,51 @@ do
     while read line
     do
         # Chapter headers are all-caps and appear by themselves e.g. "ARYA\n"
-        pov=$(echo $line | egrep "^[A-Z]{2,}$")
+        pov=$(echo $line | egrep "^([A-Z][ ]?){2,}+$")
+        pov=${pov// /_}
 
-        # New chapter?
-        if [ ! -z ${pov} ]
+        # Content line or chapter header?
+        if [ -z ${pov} ]
         then
-            # Skip front-matter
-            if [ ${pov} == 'CONTENT' ]
-            then
-                OUTFILE=/dev/null
-                continue
-            fi
-
-            # Nothing after appendix
-            if [ ${pov} == 'APPENDIX' ]
-            then
-                break
-            fi
-
-            # Increment book chapter number
-            ((CHAP_COUNT++))
-
-            # Increment character chapter number
-            if [ ! ${POV_COUNTS[${pov}]} ]
-            then
-                POV_COUNTS[${pov}]=1
-            else
-                ((POV_COUNTS[${pov}]++))
-            fi
-
-            # Change output destination
-            OUTFILE=${DIR_ASOIAF}/${BOOKNAME}/${SHORTNAME}
-            OUTFILE+=_$(printf "%02d" ${CHAP_COUNT})_${pov}
-            OUTFILE+=_$(printf "%02d" ${POV_COUNTS[${pov}]}).txt
-
-            # Print chapter
-            if [ VERBOSE ]
-            then
-                printf "|-> (%02d) %s\n" ${CHAP_COUNT} $pov
-            fi
-
-        else
-            # echo $OUTFILE
             echo $line >> ${OUTFILE}
+        else
+            # Have we reached the beginning?
+            if [ ${in_book} -eq 0 ] && [ ${pov} == 'PROLOGUE' ]
+            then
+                in_book=1
+            fi
+
+            # Are we in the content?
+            if [ ${in_book} -ne 0 ]
+            then
+                # Nothing after appendix
+                if [ ${pov} == 'APPENDIX' ]
+                then
+                    break
+                fi
+
+                # Increment book chapter number
+                ((CHAP_COUNT++))
+
+                # Increment character chapter number
+                if [ ! ${POV_COUNTS[${pov}]} ]
+                then
+                    POV_COUNTS[${pov}]=1
+                else
+                    ((POV_COUNTS[${pov}]++))
+                fi
+
+                # Change output destination
+                OUTFILE=${DIR_ASOIAF}/${BOOKNAME}/${SHORTNAME}
+                OUTFILE+=_$(printf "%02d" ${CHAP_COUNT})_${pov}
+                OUTFILE+=_$(printf "%02d" ${POV_COUNTS[${pov}]}).txt
+
+                # Print chapter
+                if [ ${VERBOSE} ]
+                then
+                    printf "|-> (%02d) %s\n" ${CHAP_COUNT} $pov
+                fi
+            fi
         fi
     done < ${book}
 done
